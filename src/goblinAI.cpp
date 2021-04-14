c46692439d18929cda2df4c31a9a573b3049288d
clonts <59036563+clonts@users.noreply.github.com>
2021-04-14 17:24:36 -0400

    changes to goblin ai

--- src/goblinAI.cpp
+++ src/goblinAI.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <windows.h>
 #include <random>
+#include "generation.cpp"
 
 using namespace std;
 
 int gX = 18, gY = 12;
+int gXL = x - 1; // to the left of goblin
+int gXR = x + 1; // to the right of goblin
+int gYB = y - 1; // below goblin
+int gYA = y + 1; // above goblin
 
 int randNum;
 
@@ -12,22 +17,24 @@ int goblinAI() {
   int randNum = rand() % 2;
 
   if(randNum == 1) {
-   if(gX - x > 0) {
-     cMap[gY][gX] = '.';
-     gX--;
-   }
-   if(gX - x < 0) {
-     cMap[gY][gX] = '.';
-     gX++;
-   }
-   if(gY - y > 0) {
-     cMap[gY][gX] = '.';
-     gY--;
-   }
-   if(gY - y < 0) {
-     cMap[gY][gX] = '.';
-     gY++;
-   }
+
+    if(gX - x > 0 && x + 1 != cMap[y][x]) {
+      cMap[gY][gX] = '.';
+      gX--;
+    }
+    if(gX - x < 0 && x - 1 != cMap[y][x]) {
+      cMap[gY][gX] = '.';
+      gX++;
+    }
+    if(gY - y > 0 && gY - 1 != cMap[y][x]) {
+      cMap[gY][gX] = '.';
+      gY--;
+    }
+    if(gY - y < 0 && gY + 1 != cMap[y][x]) {
+      cMap[gY][gX] = '.';
+      gY++;
+    }
+
   }
   return 0;
 }
