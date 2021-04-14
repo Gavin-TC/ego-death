0428701d38ea4d81eaac105dfc3a9422d1995f44
clonts <59036563+clonts@users.noreply.github.com>
2021-04-14 10:08:29 -0400

    fixes to goblinAI

--- src/goblinAI.cpp
+++ src/goblinAI.cpp
@@ -5,7 +5,23 @@ using namespace std;
 
 int gX = 18, gY = 12;
 
+DWORD lastGoblinMoveTime = 0;
+
 int goblinAI() {
 
+  DWORD dwTime = GetTickCount();
+  DWORD elapsed = dwTime - lastGoblinMoveTime;
+
+  if (elapsed > 500) {
+
+    lastGoblinMoveTime = dwTime;
+
+    if(gX - x > 0) {
+      cMap[gY][gX] = '.';
+      gX--;
+    } else if(gX - x < 0) {
+      cMap[gY][gX] = '.';
+      gX++;
+  }
   return 0;
 }
